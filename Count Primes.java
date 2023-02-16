153b1a8a986f51be0f28063182ef37caab1126b0
himanshu kanojia <84239428+Himanshukanojia@users.noreply.github.com>
2023-02-16 20:47:47 +0530

    Create Count Primes.java

---
+++ Count Primes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countPrimes(int n) {
+        if(n<2) return 0;
+        boolean prime[]= new boolean[n+1];
+        Arrays.fill(prime,true);
+        int count=0;
+        for(int i=2;i*i<=n;i++){
+            if(prime[i])
+            for(int j=i*2;j<=n;j+=i) prime[j]=false;
+        }
+        for(int i=2;i<n;i++){
+            if(prime[i])count++;
+        }
+        return count;
+    }
+}
