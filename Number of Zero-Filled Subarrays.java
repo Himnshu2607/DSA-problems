eabefe423b0f5b284dedbad5fb548daa09717a3f
himanshu kanojia <84239428+Himanshukanojia@users.noreply.github.com>
2023-03-22 01:03:50 +0530

    Create Number of Zero-Filled Subarrays.java

---
+++ Number of Zero-Filled Subarrays.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+    int i=0,k=0;
+    long sum=0;
+    while(i<nums.length){
+        if(nums[i]==0){
+            sum+=k+1;
+            k++;
+        }
+        else
+        k=0;
+        i++;
+    }
+    return sum;
+    }
+}
