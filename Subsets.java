98fd8e9a09dd74ef9ede82b34dfc08a67bb4838d
himanshu kanojia <84239428+Himanshukanojia@users.noreply.github.com>
2023-04-15 00:41:30 +0530

    Create Subsets.java

---
+++ Subsets.java
@@ -0,0 +1,16 @@
+class Solution {
+        public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>>ans=new ArrayList<>();
+        solve(0,nums,new ArrayList<>(),ans);
+        return ans;
+    }
+    public void solve(int ind,int[]nums,ArrayList<Integer>temp,List<List<Integer>>ans){
+        ans.add(new ArrayList<>(temp));
+        for(int i=ind;i<nums.length;i++){
+            if(i!=ind && nums[i]==nums[i-1]) continue;
+            temp.add(nums[i]);
+            solve(i+1,nums,temp,ans);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
