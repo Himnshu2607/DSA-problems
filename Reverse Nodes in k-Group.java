68d3f20f5490128d554305ce9d4d19385215f67d
himanshu kanojia <84239428+Himanshukanojia@users.noreply.github.com>
2023-05-15 12:54:29 +0530

    Create Reverse Nodes in k-Group.java

---
+++ Reverse Nodes in k-Group.java
@@ -0,0 +1,25 @@
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+       ListNode dummy = new ListNode(0);
+       dummy.next = head;
+       ListNode curr = dummy,prev = dummy,nex = dummy;
+       int count = 0;
+       while(curr.next!=null){
+           curr = curr.next;
+           count++;
+       }
+       while(count >=k){
+           curr = prev.next;
+           nex = curr.next;
+           for(int i = 1;i<k;i++){
+               curr.next = nex.next;
+               nex.next = prev.next;
+               prev.next = nex;
+               nex = curr.next;
+           }
+           prev = curr;
+           count-=k;
+       }
+       return dummy.next;
+    }
+}
