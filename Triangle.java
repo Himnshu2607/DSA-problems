e62a367fdfc8553455714a4310a758bdd90bf91f
himanshu kanojia <84239428+Himanshukanojia@users.noreply.github.com>
2023-06-05 23:46:47 +0530

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        for(int i = triangle.size() - 2;i>=0;i--){
+            for(int j =0;j<triangle.get(i).size();j++){
+                int first = triangle.get(i+1).get(j);
+                int second = triangle.get(i+1).get(j+1);
+                int min = Math.min(first,second);
+                int sum = min + triangle.get(i).get(j);
+                triangle.get(i).set(j,sum);
+            }
+        }
+        return triangle.get(0).get(0);
+    }
+}
