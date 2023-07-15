e54a5f8ddaca7449ae6399396b25e3e9c9c7eb44
himanshu kanojia <84239428+Himanshukanojia@users.noreply.github.com>
2023-07-15 17:33:01 +0530

    Create Minimum Cost Tree From Leaf Values.java

---
+++ Minimum Cost Tree From Leaf Values.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int mctFromLeafValues(int[] arr) {
+      int res=0;
+      Stack<Integer> st = new Stack<>();
+      st.push(Integer.MAX_VALUE);
+      for(int a : arr){
+          while(st.peek() <= a){
+              int val = st.pop();
+              res += val* Math.min(st.peek(),a);
+          }
+          st.push(a);
+      }  
+      while(st.size() > 2) res += st.pop() * st.peek();
+      return res;
+    }
+}
