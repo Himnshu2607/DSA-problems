c1e7c3dad74b1bde930742cc02d0b11746b8daeb
himanshu kanojia <84239428+Himanshukanojia@users.noreply.github.com>
2023-07-15 11:39:40 +0530

    Create Minimum Moves to Reach Target Score.java

---
+++ Minimum Moves to Reach Target Score.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minMoves(int target, int maxDoubles) {
+        int ans =0;
+        while(target >1 && maxDoubles>0){
+            if(target %2 == 0){
+                target /=2;
+                maxDoubles--;
+                ans++;
+            }
+            else{
+                target--;
+                ans++;
+            }
+        }
+        return ans+target-1;
+    }
+}
