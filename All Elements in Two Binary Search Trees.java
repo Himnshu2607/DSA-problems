abb38666b49f6a5bee1b6eda50f855a9734a47e1
himanshu kanojia <84239428+Himanshukanojia@users.noreply.github.com>
2023-07-29 12:20:57 +0530

    Create All Elements in Two Binary Search Trees.java

---
+++ All Elements in Two Binary Search Trees.java
@@ -0,0 +1,15 @@
+class Solution {
+    List<Integer> res = new ArrayList<>();
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        inOrder(root1);
+        inOrder(root2);
+        Collections.sort(res);
+        return res;
+    }
+    void inOrder(TreeNode root){
+        if(root == null) return;
+        inOrder(root.left);
+        res.add(root.val);
+        inOrder(root.right);
+    }
+}
